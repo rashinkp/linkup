1819dd1e6d8226bfc57d0feb4ac2c623cdab543c
Admin <rashinkp001@gmail.com>
2025-02-28 17:01:47 +0530

    backend almost completed

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+  connectDB();
+});
